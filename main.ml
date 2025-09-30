32c0aaa7f15283965eb76b350708fadb0a2cb2ff
Florent Monnier <monnier.florent@gmail.com>
2025-09-30 22:20:46 +0200

    main-2

--- main.ml
+++ main.ml
@@ -2,10 +2,11 @@ let print_head () =
   print_endline {|
 <html>
 <head>
+<title>BilingLit</title>
 <style type="text/css">
 body { color:#BBC; background:#EEE; padding:1em 1em 3em 2em; margin:0; }
 td { border:1px solid #ABC; padding:0; margin:1px; }
-p { padding:0; text-indent:1.2em; }
+p { padding:0 1px; text-indent:1.2em; }
 td.en { color:#420; }
 td.fr { color:#024; }
 
@@ -17,37 +18,46 @@ td { width:50%; vertical-align:top; }
 |};;
 
 
-let hi w =
-  ("<b> " ^ w ^ " </b>")
+let pat w = [
+  (" " ^ w ^ " "), (" <b>" ^ w ^ "</b> ");
+  (" " ^ w ^ ","), (" <b>" ^ w ^ "</b>,");
+  (" " ^ w ^ "."), (" <b>" ^ w ^ "</b>.");
+  (" " ^ w ^ "\n"), (" <b>" ^ w ^ "</b>\n");
+]
 
-let sp w =
-  (" " ^ w ^ " ")
-
-let push r v =
-  r := v :: !r;
-;;
 
 let print_para = (fun (word, trans_word, p1, p2) ->
     print_endline "<table>";
     Printf.printf "<tr><td class='fr'><b>%s</b></td><td class='en'><b>%s</b></td></tr>\n" trans_word word;
-    Printf.printf "<tr><td class='fr'>%s</td>\n<td class='en'>%s</td></tr>\n\n" p2 p1;
+    Printf.printf "<tr><td class='fr'>\n%s\n</td><td class='en'>\n%s</td></tr>\n" p2 p1;
     print_endline "</table>";
     print_newline ()
   )
 
+
 let () =
   let dh = Dict.load () in
-  let paras = Paras.load ~filename:"./The_Lost_World.html" in
+
+  let paras = [] in
+  let paras = Paras.load paras ~filename:"./The_Lost_World.html" in
+  let paras = Paras.load paras ~filename:"./The_Hound_of_the_Baskervilles.html" in
+  let paras = Paras.load paras ~filename:"./The_Confessions_of_Arsene_Lupin.html" in
+  let paras = Paras.load paras ~filename:"./Arsene_Lupin_Gentleman_Burglar.html" in
+  let paras = Paras.load paras ~filename:"./Alice_s_Adventures_in_Wonderland.html" in
+  let paras = Paras.load paras ~filename:"./The_Three_Musketeers.html" in
+  let paras = Paras.load paras ~filename:"./Treasure_Island.html" in
+  let paras = Paras.load paras ~filename:"./Camille.html" in
+
   print_head ();
 
   Hashtbl.iter (fun word translations ->
     List.iter (fun (p1, p2) ->
       let p1, p2 = (p2, p1) in
-      match Strings.replace p1 [(sp word, hi word)] with
+      match Strings.replace p1 (pat word) with
       | None -> ()
       | Some p1 ->
           List.iter (fun trans_word ->
-            match Strings.replace p2 [(sp trans_word, hi trans_word)] with
+            match Strings.replace p2 (pat trans_word) with
             | None -> ()
             | Some p2 ->
                 print_para (word, trans_word, p1, p2)
@@ -55,9 +65,5 @@ let () =
           ) translations;
     ) paras;
   ) dh;
-
-  (*
-  List.iter print_para !r;
-  *)
 
   ()
