3f9423a882ee44e3169849a3fac336485b762f45
Florent Monnier <monnier.florent@gmail.com>
2025-09-30 22:21:47 +0200

    main-6

--- main.ml
+++ main.ml
@@ -3,6 +3,7 @@ let pat w = [
   (" " ^ w ^ " "), (" <b>" ^ w ^ "</b> ");
   (" " ^ w ^ ","), (" <b>" ^ w ^ "</b>,");
   (" " ^ w ^ "."), (" <b>" ^ w ^ "</b>.");
+  ("'" ^ w ^ " "), ("'<b>" ^ w ^ "</b> ");
   (" " ^ w ^ "\n"), (" <b>" ^ w ^ "</b>\n");
   ("\n" ^ w ^ " "), ("\n<b>" ^ w ^ "</b> ");
 ]
@@ -31,7 +32,7 @@ let string_para1 (word, trans_word, p1, p2) =
     Printf.sprintf "<table>\n";
     Printf.sprintf "<!-- %s / %s -->\n" trans_word word;
     Printf.sprintf "<tr><td class='fr'>\n%s\n</td><td class='en'>\n%s\n</td></tr>\n" p1 p2;
-    Printf.sprintf "</table>\n";
+    Printf.sprintf "</table>\n\n";
   ]
 ;;
 
@@ -40,24 +41,36 @@ let string_para2 (word, trans_word, p1, p2) =
     Printf.sprintf "<table>\n";
     Printf.sprintf "<!-- %s / %s -->\n" word trans_word;
     Printf.sprintf "<tr><td class='en'>\n%s\n</td><td class='fr'>\n%s\n</td></tr>\n" p2 p1;
-    Printf.sprintf "</table>\n";
+    Printf.sprintf "</table>\n\n";
   ]
 ;;
 
 let file_names (word, trans_word, p1, p2) =
+  (*
   let sum = Digest.to_hex (Digest.string (p1 ^ p2)) in
-  let sum = String.sub sum 0 6 in
+  let sum = String.sub sum 0 5 in
   let esc = CharEsc.esc in
   (Printf.sprintf "./fr/w-%s-%s.html" (esc trans_word) sum,
    Printf.sprintf "./en/w-%s-%s.html" (esc word) sum)
+  *)
+  (*
+  (Printf.sprintf "/tmp/fr/%s+%s.html" trans_word word,
+   Printf.sprintf "/tmp/en/%s+%s.html" word trans_word)
+  *)
+  (Printf.sprintf "/tmp/fr/%s" trans_word,
+   Printf.sprintf "/tmp/en/%s" word)
 ;;
 
 let print_para weq =
   let p1 = string_para1 weq in
   let p2 = string_para2 weq in
   let f1, f2 = file_names weq in
+  (*
   Io.write_file ~filename:f1 p1;
   Io.write_file ~filename:f2 p2;
+  *)
+  Io.append_to_file ~filename:f1 p1;
+  Io.append_to_file ~filename:f2 p2;
 ;;
 
 
@@ -76,12 +89,15 @@ let books = [
 
 let h1 = Hashtbl.create 100
 let h2 = Hashtbl.create 100
-let count h w =
+let num h w =
   match Hashtbl.find_opt h w with
-  | None -> Hashtbl.add h w 1; 1
-  | Some n ->
-      Hashtbl.replace h w (n + 1);
-      (n + 1)
+  | None -> 0
+  | Some n -> n
+
+let increm h w =
+  match Hashtbl.find_opt h w with
+  | None -> Hashtbl.add h w 1
+  | Some n -> Hashtbl.replace h w (n + 1)
 
 
 
@@ -94,6 +110,9 @@ let () =
     ) [] books
   in
 
+  Random.self_init ();
+  let paras = List.sort (fun _ _ -> (Random.int 3) - 1) paras in
+
   let n1 = List.length ds in
   let n2 = List.length paras in
   let n = n1 * n2 in
@@ -103,22 +122,24 @@ let () =
   Printf.printf "%d paras\n" n2;
 
   List.iter (fun (word, translations) ->
-    List.iter (fun (p1, p2) ->
+    List.iter (fun (p1, p2, title, id) ->
       incr i;
       Printf.printf " > %2.3f %%\r%!" (((float !i) /. (float n)) *. 100.0);
-      if count h1 word > 16 then () else
+      if num h1 word > 22 then () else
       match Strings.replace p2 (pat word) with
       | None -> ()
       | Some p2 ->
           List.iter (fun trans_word ->
-            if count h2 trans_word > 16 then () else
+            if num h2 trans_word > 22 then () else
             match Strings.replace p1 (pat trans_word) with
             | None -> ()
             | Some p1 ->
+                increm h1 word;
+                increm h2 trans_word;
                 print_para (word, trans_word, p1, p2)
 
           ) translations;
     ) paras;
   ) ds;
 
-  ()
+  print_newline ()
