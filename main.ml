cf8e0435da2e87194e9f8bed16b20ed2d47da8d2
Florent Monnier <monnier.florent@gmail.com>
2025-09-30 22:22:21 +0200

    main-last

--- main.ml
+++ main.ml
@@ -1,76 +1,56 @@
 
+let cap = String.capitalize_ascii
+
 let pat w = [
   (" " ^ w ^ " "), (" <b>" ^ w ^ "</b> ");
   (" " ^ w ^ ","), (" <b>" ^ w ^ "</b>,");
   (" " ^ w ^ "."), (" <b>" ^ w ^ "</b>.");
   ("'" ^ w ^ " "), ("'<b>" ^ w ^ "</b> ");
+  ("’" ^ w ^ " "), ("’<b>" ^ w ^ "</b> ");
+  (" " ^ w ^ "’"), (" <b>" ^ w ^ "</b>’");
+  (" " ^ w ^ "s "), (" <b>" ^ w ^ "s</b> ");
   (" " ^ w ^ "\n"), (" <b>" ^ w ^ "</b>\n");
   ("\n" ^ w ^ " "), ("\n<b>" ^ w ^ "</b> ");
+  ((cap w) ^ " "), ("<b>" ^ (cap w) ^ "</b> ");
 ]
 
-(*
-let string_para1 (word, trans_word, p1, p2) =
-  String.concat "" [
-    Printf.sprintf "<table>\n";
-    Printf.sprintf "<tr><td class='fr'><b>%s</b></td><td class='en'><b>%s</b></td></tr>\n" trans_word word;
-    Printf.sprintf "<tr><td class='fr'>\n%s\n</td><td class='en'>\n%s\n</td></tr>\n" p1 p2;
-    Printf.sprintf "</table>\n";
-  ]
-;;
-
-let string_para2 (word, trans_word, p1, p2) =
-  String.concat "" [
-    Printf.sprintf "<table>\n";
-    Printf.sprintf "<tr><td class='en'><b>%s</b></td><td class='fr'><b>%s</b></td></tr>\n" word trans_word;
-    Printf.sprintf "<tr><td class='en'>\n%s\n</td><td class='fr'>\n%s\n</td></tr>\n" p2 p1;
-    Printf.sprintf "</table>\n";
-  ]
-;;
-*)
-let string_para1 (word, trans_word, p1, p2) =
+let string_para1 (word, trans_word, p1, p2, file, title1, title2, id) =
   String.concat "" [
     Printf.sprintf "<table>\n";
     Printf.sprintf "<!-- %s / %s -->\n" trans_word word;
     Printf.sprintf "<tr><td class='fr'>\n%s\n</td><td class='en'>\n%s\n</td></tr>\n" p1 p2;
+    Printf.sprintf "<tr><td class='from en'>\n<a class='from' href='%s#%s'>%s</a>\n</td><td class='from fr'>\n" file id title1;
+    Printf.sprintf "<a class='from' href='%s#%s'>%s</a>\n</td></tr>\n" file id title2;
     Printf.sprintf "</table>\n\n";
   ]
 ;;
 
-let string_para2 (word, trans_word, p1, p2) =
+let string_para2 (word, trans_word, p1, p2, file, title1, title2, id) =
   String.concat "" [
     Printf.sprintf "<table>\n";
     Printf.sprintf "<!-- %s / %s -->\n" word trans_word;
     Printf.sprintf "<tr><td class='en'>\n%s\n</td><td class='fr'>\n%s\n</td></tr>\n" p2 p1;
+    Printf.sprintf "<tr><td class='from en'>\n<a class='from' href='%s#%s'>%s</a>\n</td><td class='from fr'>\n" file id title2;
+    Printf.sprintf "<a class='from' href='%s#%s'>%s</a>\n</td></tr>\n" file id title1;
     Printf.sprintf "</table>\n\n";
   ]
 ;;
 
-let file_names (word, trans_word, p1, p2) =
-  (*
-  let sum = Digest.to_hex (Digest.string (p1 ^ p2)) in
-  let sum = String.sub sum 0 5 in
-  let esc = CharEsc.esc in
-  (Printf.sprintf "./fr/w-%s-%s.html" (esc trans_word) sum,
-   Printf.sprintf "./en/w-%s-%s.html" (esc word) sum)
-  *)
-  (*
-  (Printf.sprintf "/tmp/fr/%s+%s.html" trans_word word,
-   Printf.sprintf "/tmp/en/%s+%s.html" word trans_word)
-  *)
+
+let file_names (word, trans_word, p1, p2, _, _, _, _) =
   (Printf.sprintf "/tmp/fr/%s" trans_word,
    Printf.sprintf "/tmp/en/%s" word)
 ;;
+
 
 let print_para weq =
   let p1 = string_para1 weq in
   let p2 = string_para2 weq in
   let f1, f2 = file_names weq in
-  (*
-  Io.write_file ~filename:f1 p1;
-  Io.write_file ~filename:f2 p2;
-  *)
-  Io.append_to_file ~filename:f1 p1;
-  Io.append_to_file ~filename:f2 p2;
+  try
+    Io.append_to_file ~filename:f1 p1;
+    Io.append_to_file ~filename:f2 p2;
+  with _ -> ()
 ;;
 
 
@@ -111,6 +91,7 @@ let () =
   in
 
   Random.self_init ();
+  let ds = List.sort (fun _ _ -> (Random.int 3) - 1) ds in
   let paras = List.sort (fun _ _ -> (Random.int 3) - 1) paras in
 
   let n1 = List.length ds in
@@ -122,7 +103,7 @@ let () =
   Printf.printf "%d paras\n" n2;
 
   List.iter (fun (word, translations) ->
-    List.iter (fun (p1, p2, title, id) ->
+    List.iter (fun (p1, p2, file, title1, title2, id) ->
       incr i;
       Printf.printf " > %2.3f %%\r%!" (((float !i) /. (float n)) *. 100.0);
       if num h1 word > 22 then () else
@@ -136,7 +117,8 @@ let () =
             | Some p1 ->
                 increm h1 word;
                 increm h2 trans_word;
-                print_para (word, trans_word, p1, p2)
+                Printf.printf "\n > match found for: %s / %s\n%!" word trans_word;
+                print_para (word, trans_word, p1, p2, file, title1, title2, id)
 
           ) translations;
     ) paras;
