ea0a1cc62552f7b1268d6fba3ada9673d2373d8e
Florent Monnier <monnier.florent@gmail.com>
2025-10-02 21:22:30 +0200

    addition book download for v3

---
+++ src/books/download2.sh
@@ -0,0 +1,5 @@
+wget http://decapode314.free.fr/bilingual-eq3/books/20_000_Leagues_Under_the_Seas.html
+wget http://decapode314.free.fr/bilingual-eq3/books/A_Study_in_Scarlet.html
+wget http://decapode314.free.fr/bilingual-eq3/books/Dracula.html
+wget http://decapode314.free.fr/bilingual-eq3/books/Dr_Jekyll_and_Mr_Hyde.html
+wget http://decapode314.free.fr/bilingual-eq3/books/Pride_and_Prejudice.html
